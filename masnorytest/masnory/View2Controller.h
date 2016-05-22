ff0ff902b4cc7b1f97e22fc4f764ac0b45879c44
刘衍 <848063757@qq.com>
2016-05-22 16:33:04 +0800

    提交

---
+++ masnorytest/masnory/View2Controller.h
@@ -0,0 +1,13 @@
+//
+//  View2Controller.h
+//  masnory
+//
+//  Created by Yanni on 16/5/22.
+//  Copyright © 2016年 JM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface View2Controller : ViewController
+
+@end
